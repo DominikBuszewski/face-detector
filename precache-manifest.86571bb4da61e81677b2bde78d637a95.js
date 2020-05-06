b6dc8409d136091ee1fa6ab19316a127e15a9b96
Dominik Buszewski <yogiasdasd@gmail.com>
2020-05-06 17:44:48 +0200

    Updates

---
+++ precache-manifest.86571bb4da61e81677b2bde78d637a95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83b5dce06622830af66bdab9ac45af6e",
+    "url": "/face-detector/index.html"
+  },
+  {
+    "revision": "d68949550cf9e6477652",
+    "url": "/face-detector/static/css/main.75717cd0.chunk.css"
+  },
+  {
+    "revision": "d6e7ef81977dd45be1ec",
+    "url": "/face-detector/static/js/2.545730e2.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/face-detector/static/js/2.545730e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d68949550cf9e6477652",
+    "url": "/face-detector/static/js/main.f85364af.chunk.js"
+  },
+  {
+    "revision": "c9c04dc2712fca12de82",
+    "url": "/face-detector/static/js/runtime-main.7b864f13.js"
+  }
+]);
